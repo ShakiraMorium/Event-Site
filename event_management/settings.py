2a9ba51233e2d317da60495f220c112cb3fc23f6
event_management <shamimasham7@gmail.com>
2025-08-05 23:20:42 +0600

    event added

--- event_management/settings.py
+++ event_management/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['httpS://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
